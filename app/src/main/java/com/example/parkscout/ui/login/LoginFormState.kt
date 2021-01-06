d02aa41f62f2d9c97d468fcbf2c3089f36fcf305
adibigler <adibigler96@gmail.com>
2021-01-06 23:14:47 +0200

    logIn + register
    ui only

---
+++ app/src/main/java/com/example/parkscout/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package com.example.parkscout.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)

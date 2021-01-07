1906e7457f3efe21609e44fbf20c9b1c0a242660
Tomer <bboypsyfly@gmail.com>
2021-01-07 21:18:44 +0200

    Navigation completed

--- app/src/main/java/com/example/parkscout/MainActivity.kt
+++ app/src/main/java/com/example/parkscout/MainActivity.kt
@@ -16,13 +16,33 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        // Setup the app and the bottom app bar UI.
+        setupBaseDesign()
+
+        // Navigation
+        setupNavigation()
+    }
+
+    fun setupNavigation() {
+
+        // Bottom App Bar Navigation
+        val navController = Navigation.findNavController(this, R.id.main_navhost_frag)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+
+        // Handle FAB navigation
+        fab.setOnClickListener{view ->
+            navController.navigate(R.id.action_global_addParkFragment)
+        }
+    }
+
+    fun setupBaseDesign() {
         val attr = window.attributes
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             attr.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
         }
-        setContentView(R.layout.activity_main)
-
         // Remove the weird shadow on the menu items.
         bottomNavigationView.background = null
 
@@ -33,54 +53,12 @@ class MainActivity : AppCompatActivity() {
         val radius: Float = resources.getDimension(R.dimen.default_corner_radius);
         val bottomBarBackground: MaterialShapeDrawable =
             bottomAppBar.background as MaterialShapeDrawable;
+
         bottomBarBackground.shapeAppearanceModel = bottomBarBackground.shapeAppearanceModel.toBuilder()
             .setTopRightCorner(CornerFamily.ROUNDED, radius)
             .setTopLeftCorner(CornerFamily.ROUNDED, radius)
             .setBottomRightCorner(CornerFamily.ROUNDED, radius)
             .setBottomLeftCorner(CornerFamily.ROUNDED, radius)
             .build()
-
-        // Navigation
-        val navController = Navigation.findNavController(this, R.id.main_navhost_frag)
-        NavigationUI.setupWithNavController(bottomNavigationView, navController)
-
-        // Handle FAB navigation
-        fab.setOnClickListener{view ->
-//            val navController = Navigation.findNavController(bottomNavigationView)
-            navController.navigate(R.id.action_global_addParkFragment)
-        }
-
-        // Handle Bottom Menu Navigation
-        bottomAppBar.setNavigationOnClickListener{view ->
-            Log.d("Tag", "Navigating")
-//            val navController = Navigation.findNavController(view)
-            when (view.id) {
-                R.id.nav_search -> {
-                    Log.d("Tag", "Navigating to search fragment")
-                    navController.navigate(R.id.action_global_searchFragment)
-
-                    true
-                }
-                R.id.nav_chat -> {
-                    navController.navigate(R.id.action_global_chatFragment)
-
-                    true
-                }
-                R.id.nav_profile -> {
-                    navController.navigate(R.id.action_global_profileFragment)
-
-                    true
-                }
-                R.id.nav_settings -> {
-
-                    navController.navigate(R.id.action_global_settingsFragment)
-
-                    true
-                }
-                else -> false
-            }
-        }
-
-        setSupportActionBar(bottomAppBar)
     }
 }

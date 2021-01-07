60860fcf2afe789ab69e134d272fed42b63c5599
Tomer <bboypsyfly@gmail.com>
2021-01-07 20:30:27 +0200

    Fragments Setup - Navigation not yet working

--- app/src/main/java/com/example/parkscout/MainActivity.kt
+++ app/src/main/java/com/example/parkscout/MainActivity.kt
@@ -3,33 +3,23 @@ package com.example.parkscout
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.view.WindowInsets
-import android.view.WindowInsetsController
+import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.google.android.material.shape.CornerFamily
 import com.google.android.material.shape.MaterialShapeDrawable
 import kotlinx.android.synthetic.main.activity_main.*
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-////            window.setDecorFitsSystemWindows(false)
-////            val controller = window.insetsController
-////
-////            if (controller != null) {
-////                controller.hide(WindowInsets.Type.statusBars())
-////                controller.hide(WindowInsets.Type.navigationBars())
-////                controller.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-////            }
-//
         val attr = window.attributes
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-//            window.statusBarColor = getColor(android.R.color.transparent)
             attr.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
-            Log.d("Tag", "Hello")
         }
         setContentView(R.layout.activity_main)
 
@@ -49,5 +39,48 @@ class MainActivity : AppCompatActivity() {
             .setBottomRightCorner(CornerFamily.ROUNDED, radius)
             .setBottomLeftCorner(CornerFamily.ROUNDED, radius)
             .build()
+
+        // Navigation
+        val navController = Navigation.findNavController(this, R.id.main_navhost_frag)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+
+        // Handle FAB navigation
+        fab.setOnClickListener{view ->
+//            val navController = Navigation.findNavController(bottomNavigationView)
+            navController.navigate(R.id.action_global_addParkFragment)
+        }
+
+        // Handle Bottom Menu Navigation
+        bottomAppBar.setNavigationOnClickListener{view ->
+            Log.d("Tag", "Navigating")
+//            val navController = Navigation.findNavController(view)
+            when (view.id) {
+                R.id.nav_search -> {
+                    Log.d("Tag", "Navigating to search fragment")
+                    navController.navigate(R.id.action_global_searchFragment)
+
+                    true
+                }
+                R.id.nav_chat -> {
+                    navController.navigate(R.id.action_global_chatFragment)
+
+                    true
+                }
+                R.id.nav_profile -> {
+                    navController.navigate(R.id.action_global_profileFragment)
+
+                    true
+                }
+                R.id.nav_settings -> {
+
+                    navController.navigate(R.id.action_global_settingsFragment)
+
+                    true
+                }
+                else -> false
+            }
+        }
+
+        setSupportActionBar(bottomAppBar)
     }
 }

88191e9bb5f8fff4221f24e3a021dfa191466512
adibigler <adibigler96@gmail.com>
2021-02-13 10:46:54 +0200

    trainingSpotDb

---
+++ app/src/main/java/com/example/parkscout/Repository/ModelSQL/TrainingSpotModelSQL.kt
@@ -0,0 +1,36 @@
+package com.example.parkscout.Repository.ModelSQL
+
+import android.os.AsyncTask
+import androidx.lifecycle.LiveData
+import com.example.parkscout.data.model.TrainingSpot
+
+class TrainingSpotModelSQL {
+    fun getAllParks(): LiveData<List<TrainingSpot>> {
+        return AppLocalDb.getInstance().trainingSpotDao().getAllParks();
+    }
+
+    public interface AddParkListener {
+        fun onComplete();
+    }
+
+    public fun addChatMessage(park: TrainingSpot, listener: AddParkListener) {
+        class MyAsyncTask: AsyncTask<TrainingSpot, Void, TrainingSpot>() {
+            override fun doInBackground(vararg params: TrainingSpot?): TrainingSpot {
+                AppLocalDb.getInstance().trainingSpotDao().insertPark(park);
+
+                return park;
+            }
+
+            override fun onPostExecute(result: TrainingSpot?) {
+                super.onPostExecute(result);
+
+                if (listener != null) {
+                    listener.onComplete();
+                }
+            }
+        }
+
+        var task: MyAsyncTask = MyAsyncTask();
+        task.execute();
+    }
+}

2a926cf0aea1c6af132bb4f45497930d91f8e62a
Tomer <bboypsyfly@gmail.com>
2021-03-02 23:36:16 +0200

    Chat button design

--- app/src/main/java/com/example/parkscout/Fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/parkscout/Fragment/ProfileFragment.kt
@@ -24,20 +24,9 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.example.parkscout.Repository.User
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() , OnMapReadyCallback {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    // Data Members
     private lateinit var mMap: GoogleMap
     private lateinit var viewModel: ProfileViewModel;
     private lateinit var mUserImageView: ImageView;
@@ -45,6 +34,7 @@ class ProfileFragment : Fragment() , OnMapReadyCallback {
     private lateinit var mUserDesc: TextView;
     private lateinit var mUserID: String;
 
+    // Methods
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mUserID = FirebaseAuth.getInstance().currentUser?.uid ?: "";
@@ -74,31 +64,13 @@ class ProfileFragment : Fragment() , OnMapReadyCallback {
             viewModel.user.observe(viewLifecycleOwner, { user: User ->
                 Glide.with(activity as Activity).load(user.profilePic).into(mUserImageView);
                 mUserName.text = user.name;
+                mUserDesc.text = user.description;
             });
         }
 
         return rootview
     }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+    
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 

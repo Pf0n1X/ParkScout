e2c65414e44c91a1d0e9fd0ea45f48601e3e7669
adibigler <adibigler96@gmail.com>
2021-03-05 13:30:21 +0200

    comment parser

--- app/src/main/java/com/example/parkscout/Repository/Comment.kt
+++ app/src/main/java/com/example/parkscout/Repository/Comment.kt
@@ -4,25 +4,13 @@ import androidx.room.*
 import java.time.format.DateTimeFormatter
 
 @Entity(tableName = "comments",primaryKeys = arrayOf("trainingId","userId") )
-//@Entity(tableName = "comments" ,
-//    foreignKeys = arrayOf(ForeignKey(entity = TrainingSpot::class,
-//    parentColumns = arrayOf("parkId"),
-//    childColumns = arrayOf("trainingId"),
-//    onDelete = ForeignKey.CASCADE),
-//    ForeignKey(entity = User::class,parentColumns = arrayOf("uId"),
-//        childColumns = arrayOf("userId"),onDelete =ForeignKey.CASCADE)))
 data class Comment(
     @ColumnInfo(name = "trainingId")
-    val trainingSpotId: String,
+    var trainingSpotId: String,
     @ColumnInfo(name = "userId")
-    val userId: String,
-    val c_text: String,
-    @Embedded(prefix = "comment_") val c_dateTime: DateTimeFormatter?
+    var userId: String,
+    var c_text: String,
+    @Embedded(prefix = "comment_") var c_dateTime: DateTimeFormatter?
 ){
-//    companion object {
-//        public var instance: Comment = Comment("","","",null)
-//            public get
-//            private set;
-//    }
 
 }
